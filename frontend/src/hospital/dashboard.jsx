5962201c95a8155da4ba5f2ca230fb1d66e14b14
sarinsanyal <sarinsanyal2005@gmail.com>
2025-02-12 21:35:34 +0530

    Update and Initialised Frontend

---
+++ frontend/src/hospital/dashboard.jsx
@@ -0,0 +1,12 @@
+//Hospital Dashboard
+
+import './dashboard.css';
+import React from 'react';
+
+export default function HospitalDashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
