56757f91acb46309b9ce0ee0661f90f79473b99a
malay77patra <malay77patra@gmail.com>
2025-02-12 21:18:48 +0530

    update

---
+++ frontend/src/user/dashboard.jsx
@@ -0,0 +1,12 @@
+//User Dashboard
+
+import './dashboard.css';
+import React from 'react';
+
+export default function UserDashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
